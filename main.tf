3e7abb84eede551ee2327a81e7994fb620e4ddac
sivadotblog <hello@siva.blog>
2022-08-22 09:39:19 -0400

    updated instance

--- main.tf
+++ main.tf
@@ -17,8 +17,8 @@ module "databricks_workspace_management" {
   spark_version = data.databricks_spark_version.latest_lts.id
   #  node_type_id                = var.node_type_id == "smallest" ? local.smallest_node_type : var.node_type_id                      #var.node_type_id
   #  driver_node_type_id         = var.driver_node_type_id == "smallest" ? local.smallest_driver_node_type : var.driver_node_type_id #var.driver_node_type_id
-  node_type_id                = data.databricks_node_type.smallest1.id
-  driver_node_type_id         = data.databricks_node_type.smallest1.id
+  node_type_id                = "Standard_DS3_v2"
+  driver_node_type_id         = "Standard_DS3_v2"
   num_workers                 = var.num_workers
   auto_scaling                = var.auto_scaling
   autotermination_minutes     = var.autotermination_minutes
